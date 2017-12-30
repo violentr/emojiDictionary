961c1eff2d995907b3cc236285fbe430aa6485a4
Deniss <den.b@itservicesgroup.co.uk>
2017-12-30 19:00:04 +0000

    Add Array of emojis listed in tableView

---
+++ swift app/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  swift app
+//
+//  Created by den on 30/12/2017.
+//  Copyright © 2017 den. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+  @IBOutlet weak var mainscreenTableView: UITableView!
+
+ var emojis = ["😀","😬","😂", "😃", "😄"]
+
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+   mainscreenTableView.dataSource = self
+   mainscreenTableView.delegate = self
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+  return emojis.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+   
+      cell.textLabel?.text = emojis[indexPath.row]
+      return cell
+
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
