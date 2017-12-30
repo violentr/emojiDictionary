046f03124ee16e02ba6afc7d2e7c2fddf9693799
Deniss <den.b@itservicesgroup.co.uk>
2017-12-30 19:58:55 +0000

    Add Embed Navigation controller, add Segue

--- swift app/ViewController.swift
+++ swift app/ViewController.swift
@@ -33,6 +33,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
       return cell
 
   }
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+   performSegue(withIdentifier: "moveSegue", sender: "🤑")
+  }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
